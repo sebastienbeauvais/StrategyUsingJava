02825956013c10ef0529eee0df0e4ee7a1d1e0eb
Sebastien Beauvais <sbeauvais7@gmail.com>
2026-03-17 16:46:37 -0700

    Initial commit for project

---
+++ src/main/java/designPatterns/example/StrategyPattern/Repository/StoreRepository.java
@@ -0,0 +1,5 @@
+package designPatterns.example.StrategyPattern.Repository;
+
+public class StoreRepository {
+    
+}
---
+++ src/test/java/designPatterns/example/StrategyPattern/ApplicationTests.java
@@ -0,0 +1,13 @@
+package designPatterns.example.StrategyPattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
